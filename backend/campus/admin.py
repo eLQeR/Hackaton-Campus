e3d42fe2bce6ec8e06b518fdfd243948b21e40c9
eLQeR <131271125+eLQeR@users.noreply.github.com>
2024-03-02 19:12:42 +0200

    Innitial database, added models and views and JWT token for authorization, refactor admin panel

---
+++ backend/campus/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from .models import User, Specialty, Faculty, Group, University, Course
+
+# class GalleryInline(admin.TabularInline):
+#     fk_name = 'item'
+#     model = Gallery
+#     extra = 5
+#
+#
+# class VariantOfItemInline(admin.TabularInline):
+#     fk_name = 'item'
+#     model = VariantOfItem
+#     extra = 5
+#
+#
+# @admin.register(Item)
+# class ProductAdmin(admin.ModelAdmin):
+#     exclude = ["article"]
+#     inlines = [GalleryInline, VariantOfItemInline]
+
+
+admin.site.register(User)
+admin.site.register(Faculty)
+admin.site.register(Specialty)
+admin.site.register(Group)
+admin.site.register(University)
+admin.site.register(Course)
--- backend/campus_system/settings.py
+++ backend/campus_system/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -37,6 +36,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "campus",
+    "rest_framework",
+    "rest_framework_simplejwt",
+    "corsheaders",
+
 ]
 
 MIDDLEWARE = [
@@ -69,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'campus_system.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -79,7 +82,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -99,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -111,7 +112,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
@@ -121,3 +121,22 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+AUTH_USER_MODEL = "campus.User"
