849f500b6e633443e62048e62be995def6e93245
Naz <ngvoznyuk@gmail.com>
2024-03-03 05:18:44 +0200

    test styles

--- frontend/src/pages/teacher/CreateTestPage.jsx
+++ frontend/src/pages/teacher/CreateTestPage.jsx
@@ -65,164 +65,6 @@ const CreateTestPage = () => {
         })
     }
 
-    const temp = async (i) => {
-        try {
-            await axiosPrivate.post(
-                '/create-test/',
-                JSON.stringify({
-                    name: 'Столиці світу Яроса',
-                    description: 'Це тест на знання столиць світу',
-                    test_time: '13:30:00',
-                    max_mark: 20,
-                    group: i,
-                    questions: [
-                        {
-                            question: 'Столиця України?',
-                            test: 8,
-                            mark: 2,
-                            variants: [
-                                {
-                                    answer: 'a) Донецьк',
-                                    is_correct: 'False',
-                                    question: 8,
-                                },
-                                {
-                                    answer: 'a) Вінниця',
-                                    is_correct: 'False',
-                                    question: 8,
-                                },
-                                {
-                                    answer: 'a) Київ',
-                                    is_correct: 'True',
-                                    question: 8,
-                                },
-                                {
-                                    answer: 'a) Харків',
-                                    is_correct: 'False',
-                                    question: 8,
-                                },
-                            ],
-                        },
-                        {
-                            question: 'Столиця Франції?',
-                            test: 8,
-                            mark: 2,
-                            variants: [
-                                {
-                                    answer: 'a) Київ',
-                                    is_correct: 'False',
-                                    question: 8,
-                                },
-                                {
-                                    answer: 'a) Тегеран',
-                                    is_correct: 'False',
-                                    question: 8,
-                                },
-                                {
-                                    answer: 'a) Берлін',
-                                    is_correct: 'False',
-                                    question: 8,
-                                },
-                                {
-                                    answer: 'a) Париж',
-                                    is_correct: 'True',
-                                    question: 8,
-                                },
-                            ],
-                        },
-                        {
-                            question: 'Столиця Німеччини?',
-                            test: 8,
-                            mark: 2,
-                            variants: [
-                                {
-                                    answer: 'a) Київ',
-                                    is_correct: 'False',
-                                    question: 8,
-                                },
-                                {
-                                    answer: 'a) Рига',
-                                    is_correct: 'False',
-                                    question: 8,
-                                },
-                                {
-                                    answer: 'a) Берлін',
-                                    is_correct: 'True',
-                                    question: 8,
-                                },
-                                {
-                                    answer: 'a) Париж',
-                                    is_correct: 'False',
-                                    question: 8,
-                                },
-                            ],
-                        },
-                        {
-                            question: 'Столиця Німеччини?',
-                            test: 8,
-                            mark: 2,
-                            variants: [
-                                {
-                                    answer: 'a) Київ',
-                                    is_correct: 'False',
-                                    question: 8,
-                                },
-                                {
-                                    answer: 'a) Рига',
-                                    is_correct: 'False',
-                                    question: 8,
-                                },
-                                {
-                                    answer: 'a) Берлін',
-                                    is_correct: 'True',
-                                    question: 8,
-                                },
-                                {
-                                    answer: 'a) Париж',
-                                    is_correct: 'False',
-                                    question: 8,
-                                },
-                            ],
-                        },
-                        {
-                            question: 'Столиця Німеччини?',
-                            test: 8,
-                            mark: 2,
-                            variants: [
-                                {
-                                    answer: 'a) Київ',
-                                    is_correct: 'False',
-                                    question: 8,
-                                },
-                                {
-                                    answer: 'a) Рига',
-                                    is_correct: 'False',
-                                    question: 8,
-                                },
-                                {
-                                    answer: 'a) Берлін',
-                                    is_correct: 'True',
-                                    question: 8,
-                                },
-                                {
-                                    answer: 'a) Париж',
-                                    is_correct: 'False',
-                                    question: 8,
-                                },
-                            ],
-                        },
-                    ],
-                })
-            )
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    for (let i = 6; i < 50; i++) {
-        temp(i)
-    }
-
     const updateAnswers = (id, index) => (e) => {
         setFormData((prev) => {
             const newData = { ...prev }
@@ -266,129 +108,134 @@ const CreateTestPage = () => {
     if (loading) return <Loader />
 
     return (
-        <div>
-            <button
-                onClick={() => {
-                    setQuestions((prev) => [...prev, { id: prev.length }])
-                }}
+        <section>
+            <form onSubmit={submit}>
+                <div id={'task-head'}>
+                    <select className={'left-side'} onChange={handleFormChange} name="group" required>
+                        {groups.map((group) => (
+                            <option key={group.id} value={group.id}>
+                                {group.code}
+                            </option>
+                        ))}
+                    </select>
+                    <input className={'element'}
+                           onChange={handleFormChange}
+                           name="name"
+                           type="text"
+                           placeholder="Назва тесту"
+                           required
+                    />
+                    <textarea className={'element'}
+                              onChange={handleFormChange}
+                              name="description"
+                              placeholder="Опис тесту"
+                              required
+                    />
+                    <input className={'element'}
+                           onChange={handleFormChange}
+                           name="test_time"
+                           placeholder="Час на виконання"
+                           type="number"
+                           required
+                    />
+                    <input className={'element right-side'}
+                           onChange={handleFormChange}
+                           name="max_mark"
+                           placeholder="Максимальний бал"
+                           type="number"
+                           required
+                    />
+                </div>
+                <div className={'task-flex'}>
+                    {questions.map((question) => (
+                        <div key={question.id} id={question.id} className={'task-body'}>
+                            <input
+                                onChange={updateForm(question.id)}
+                                name={`question`}
+                                type="text"
+                                placeholder="Текст запитання"
+                                required
+                            />
+                            <input
+                                onChange={updateForm(question.id)}
+                                name={`mark`}
+                                type="number"
+                                placeholder="Оцінка"
+                                required
+                            />
+                            <input
+                                onChange={updateAnswers(question.id, 0)}
+                                type="text"
+                                name="variant1"
+                                placeholder="Варіант 1"
+                                required
+                            />
+                            <input
+                                onChange={updateAnswers(question.id, 1)}
+                                type="text"
+                                name="variant2"
+                                placeholder="Варіант 2"
+                                required
+                            />
+                            <input
+                                onChange={updateAnswers(question.id, 2)}
+                                type="text"
+                                name="variant3"
+                                placeholder="Варіант 3"
+                                required
+                            />
+                            <input
+                                onChange={updateAnswers(question.id, 3)}
+                                type="text"
+                                name="variant4"
+                                placeholder="Варіант 4"
+                                required
+                            />
+                            <label htmlFor="answer">
+                                Оберіть правильну відповідь
+                            </label>
+                            <select
+                                onChange={handleAnswerSelect(question.id)}
+                                name="answer"
+                            >
+                                <option></option>
+                                <option value={0}>1</option>
+                                <option value={1}>2</option>
+                                <option value={2}>3</option>
+                                <option value={3}>4</option>
+                            </select>
+                            <button
+                                type="button"
+                                onClick={() => {
+                                    setQuestions((prev) =>
+                                        prev.filter(
+                                            (item) => item.id !== question.id
+                                        )
+                                    )
+                                    setFormData((prev) => {
+                                        const newData = {...prev}
+                                        delete newData.questions[question.id]
+                                        return newData
+                                    })
+                                }}
+                            >
+                                Видалити
+                            </button>
+                        </div>
+                    ))}
+                    <button type="submit" id={'test-created'}>
+                        Завершити створення
+                    </button>
+                </div>
+            </form>
+            <button id={'add-task'}
+                    onClick={() => {
+                        setQuestions((prev) => [...prev, {id: prev.length}])
+                    }}
             >
                 Додати питання
             </button>
-            <form onSubmit={submit}>
-                <select onChange={handleFormChange} name="group" required>
-                    {groups.map((group) => (
-                        <option key={group.id} value={group.id}>
-                            {group.code}
-                        </option>
-                    ))}
-                </select>
-
-                <input
-                    onChange={handleFormChange}
-                    name="name"
-                    type="text"
-                    placeholder="Назва тесту"
-                    required
-                />
-                <textarea
-                    onChange={handleFormChange}
-                    name="description"
-                    placeholder="Опис тесту"
-                    required
-                />
-                <input
-                    onChange={handleFormChange}
-                    name="test_time"
-                    placeholder="Час на виконання"
-                    type="number"
-                    required
-                />
-                <input
-                    onChange={handleFormChange}
-                    name="max_mark"
-                    placeholder="Максимальний бал"
-                    type="number"
-                    required
-                />
-                {questions.map((question) => (
-                    <div key={question.id} id={question.id}>
-                        <input
-                            onChange={updateForm(question.id)}
-                            name={`question`}
-                            type="text"
-                            placeholder="Текст запитання"
-                            required
-                        />
-                        <input
-                            onChange={updateForm(question.id)}
-                            name={`mark`}
-                            type="number"
-                            placeholder="Оцінка"
-                            required
-                        />
-                        <input
-                            onChange={updateAnswers(question.id, 0)}
-                            type="text"
-                            name="variant1"
-                            placeholder="Варіант 1"
-                            required
-                        />
-                        <input
-                            onChange={updateAnswers(question.id, 1)}
-                            type="text"
-                            name="variant2"
-                            placeholder="Варіант 2"
-                            required
-                        />
-                        <input
-                            onChange={updateAnswers(question.id, 2)}
-                            type="text"
-                            name="variant3"
-                            placeholder="Варіант 3"
-                            required
-                        />
-                        <input
-                            onChange={updateAnswers(question.id, 3)}
-                            type="text"
-                            name="variant4"
-                            placeholder="Варіант 4"
-                            required
-                        />
-                        <label htmlFor="answer">
-                            Оберіть правильну відповідь
-                        </label>
-                        <select
-                            onChange={handleAnswerSelect(question.id)}
-                            name="answer"
-                        >
-                            <option></option>
-                            <option value={0}>1</option>
-                            <option value={1}>2</option>
-                            <option value={2}>3</option>
-                            <option value={3}>4</option>
-                        </select>
-                        <button
-                            type="button"
-                            onClick={() => {
-                                setQuestions((prev) =>
-                                    prev.filter(
-                                        (item) => item.id !== question.id
-                                    )
-                                )
-                                setFormData((prev) => {
-                                    const newData = { ...prev }
-                                    delete newData.questions[question.id]
-                                    return newData
-                                })
-                            }}
-                        >
-                            Видалити
-                        </button>
-                    </div>
-                ))}
-                <button type="submit">submit</button>
-            </form>
-        </div>
+        </section>
     )
 }
 
